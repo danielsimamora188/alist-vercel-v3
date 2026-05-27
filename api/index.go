381ba39e0ac390c1bd4793b2da470d8a7783f53e
Daniel Tulus Parsaoran Simamora <97688280+danielsimamora188@users.noreply.github.com>
2026-05-28 06:27:57 +0700

    Update index.go

--- api/index.go
+++ api/index.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os/exec"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Arahkan ke biner alist yang berjalan di local
+	// 1. Cek apakah AList sudah menyala, jika belum, jalankan
+	// Menggunakan /tmp karena itu satu-satunya folder yang bisa ditulis
+	cmd := exec.Command("/tmp/alist", "server", "--no-prefix")
+	cmd.Start()
+
+	// 2. Beri waktu singkat agar biner siap
+	time.Sleep(1 * time.Second)
+
+	// 3. Proxy request ke port default AList
 	target, _ := url.Parse("http://127.0.0.1:5244")
 	proxy := httputil.NewSingleHostReverseProxy(target)
-	
-	// Jalankan proxy
 	proxy.ServeHTTP(w, r)
 }
